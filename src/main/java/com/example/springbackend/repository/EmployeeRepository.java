9478ea6a214bac8e0470a1417ddb5d79070c299f
Akshay <akshayimmortal297@gmail.com>
2022-07-15 18:16:39 +0530

    first commit

---
+++ src/main/java/com/example/springbackend/repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.example.springbackend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.springbackend.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository <Employee, Long> {
+
+}
